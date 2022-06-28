fd32d7a2bd621ec27932fe1632b003d55beb6ea6
Asfaw Nahom <s2200108@edu.bc.fi>
2022-06-28 14:07:05 +0300

    Call back function and function inside function

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const getPlayerChoice = () => {
   const selection = prompt(`${ROCK},${PAPER},${SCISSORS}`, "").toUpperCase();
   if (selection !== ROCK && selection !== PAPER && selection !== SCISSORS) {
     alert(`Ivalid choice! We chose ${DEFAULT_USER_CHOICE} for you!`);
-    return DEFAULT_USER_CHOICE;
+    return;
   }
   return selection;
 };
@@ -29,7 +29,7 @@ const getComputerChoice = () => {
   }
 };
 
-const getWinner = (cChoice, pChoice) =>
+const getWinner = (cChoice, pChoice = DEFAULT_USER_CHOICE) =>
   cChoice === pChoice
     ? RESULT_DRAW
     : (cChoice === ROCK && pChoice === PAPER) ||
@@ -58,8 +58,27 @@ const start = () => {
   const playerChoice = getPlayerChoice();
   console.log(playerChoice);
   const computerChoice = getComputerChoice();
-  const winner = getWinner(computerChoice, playerChoice);
-  console.log(winner);
+
+  let winner;
+  if (playerChoice) {
+    winner = getWinner(computerChoice, playerChoice);
+  } else {
+    winner = getWinner(computerChoice /*playerChoice*/);
+  }
+
+  let message = `You picked ${
+    playerChoice || DEFAULT_USER_CHOICE
+  }, computer picked ${computerChoice},therefore you `;
+
+  if (winner === RESULT_DRAW) {
+    message = message + "had a draw";
+  } else if (winner === RESULT_PLAYER_WINS) {
+    message = message + "won";
+  } else {
+    message = message + "lost";
+  }
+  alert(message);
+  stop();
 };
 
 const stop = () => {
@@ -78,3 +97,22 @@ const stop = () => {
 // console.dir(startGame);
 
 startGameBtn.addEventListener("click", start);
+
+//// not related to the game
+// Call back function and function inside another function
+
+const sumUp = (resultHandler, ...numbers) => {
+  const validatenumber = (number) => {
+    return isNaN(number) ? 0 : number;
+  };
+  let sum = 0;
+  for (const num of numbers) {
+    sum += validatenumber(num);
+  }
+  resultHandler(sum);
+};
+const showResult = (result) => {
+  alert("the result after adding all numbers is: " + result);
+};
+
+console.log(sumUp(showResult, 8, 2, 5, 10, 67));
